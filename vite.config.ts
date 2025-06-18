2d5b60dce9c7a0a6d6f57940d12bfc82a2135eb1
Yaroslav Lohvynenko <yaroslav.lohvynenko@caspio.com>
2025-06-18 19:32:54 +0300

    #001006: Add import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
 });

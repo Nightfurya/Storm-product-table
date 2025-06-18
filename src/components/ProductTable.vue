0ba06db892b50c56dfe20d4e9a0c5d25d48dc14c
Yaroslav Lohvynenko <yaroslav.lohvynenko@caspio.com>
2025-06-19 00:34:31 +0300

    #001010: Extract Product table rows  into separate component

--- src/components/ProductTable.vue
+++ src/components/ProductTable.vue
@@ -3,6 +3,8 @@ import { defineProps } from 'vue';
 
 import type { Product } from '@/data-models/product';
 
+import ProductTableRow from './ProductTableRow/ProductTableRow.vue';
+
 defineProps<{
 	products: Product[];
 }>();
@@ -18,19 +20,14 @@ defineProps<{
 			<div class="product-table__row-sell">Prices</div>
 		</div>
 
-		<div class="product-table__row" v-for="product in products" :key="product.id">
-			<div class="product-table__row-sell">{{ product.id }}</div>
-			<div class="product-table__row-sell">{{ product.status }}</div>
-			<div class="product-table__row-sell">{{ product.quantity }}</div>
-			<div class="product-table__row-sell">{{ product.name }}</div>
-			<div class="product-table__row-sell">${{ product.price.toFixed(2) }}</div>
-		</div>
+		<ProductTableRow v-for="product in products" :key="product.id" :product="product" />
 	</div>
 </template>
 
 <style scoped lang="scss">
 @use '@/assets/styles/_color.vars' as colors;
 @use '@/assets/styles/_mixins' as mixins;
+@use './ProductTableRow/_ProductTableRow';
 
 .product-table {
 	@include mixins.flex-column();
@@ -38,37 +35,6 @@ defineProps<{
 	width: 100%;
 	border: 1px solid colors.$moon;
 	border-radius: 16px;
-
-	&__row {
-		width: 100%;
-		display: grid;
-		grid-template-columns: 70px 144px 125px 1fr 171px;
-		height: 56px;
-
-		&:not(:last-child) {
-			border-bottom: 1px solid colors.$moon;
-		}
-
-		&-sell {
-			@include mixins.flex-row();
-			@include mixins.font-style(14px, colors.$primary-color);
-
-			padding: 18px 16px;
-
-			line-height: 20px;
-			letter-spacing: 0;
-			vertical-align: middle;
-		}
-
-		&-sell:nth-child(4) {
-			justify-content: flex-start;
-		}
-
-		&-sell:last-child {
-			border-left: 1px solid colors.$moon;
-			justify-content: flex-end;
-		}
-	}
 
 	&__row.product-table__header {
 		&:hover {
@@ -83,10 +49,7 @@ defineProps<{
 				text-transform: uppercase;
 			}
 
-			&:nth-child(4) {
-				justify-content: center;
-			}
-
+			&:nth-child(4),
 			&:last-child {
 				justify-content: center;
 			}
---
+++ src/components/ProductTableRow/ProductTableRow.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import type { Product } from '@/data-models/product';
+
+defineProps<{
+	product: Product;
+}>();
+</script>
+
+<template>
+	<div class="product-table__row">
+		<div class="product-table__row-sell">{{ product.id }}</div>
+		<div class="product-table__row-sell">{{ product.status }}</div>
+		<div class="product-table__row-sell">{{ product.quantity }}</div>
+		<div class="product-table__row-sell">
+			<p>{{ product.name }}</p>
+			<p class="product-table__row-sell-subtitle">{{ product.serial }}</p>
+		</div>
+		<div class="product-table__row-sell">${{ product.price.toFixed(2) }}</div>
+	</div>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/_color.vars' as colors;
+@use '@/assets/styles/_mixins' as mixins;
+@use './_ProductTableRow';
+
+.product-table__row-sell {
+	&-subtitle {
+		@include mixins.font-style(12px, colors.$secondary-color);
+
+		line-height: 20px;
+		letter-spacing: 0;
+		vertical-align: middle;
+	}
+}
+</style>

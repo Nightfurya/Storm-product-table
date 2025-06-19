ac8c05455efd0b821ad7a4a769c92d583a7b8fcd
Yaroslav Lohvynenko <yaroslav.lohvynenko@caspio.com>
2025-06-19 22:21:30 +0300

    #002003: Customize ProductDetailedInfoModal + add responsive for Modal

--- src/components/ProductDetailedInfoModal/ProductDetailedInfoModal.vue
+++ src/components/ProductDetailedInfoModal/ProductDetailedInfoModal.vue
@@ -14,14 +14,189 @@ defineEmits(['close']);
 <template>
 	<Modal :selectedItem="selectedProduct">
 		<template v-slot:[ModalPart.HEADER]>
-			<button class="modal__close" @click="$emit('close')">✕</button>
+			<div class="product-modal__header">
+				<h6 class="product-modal__header-title">{{ selectedProduct.name }}</h6>
+				<button class="product-modal__header-close-button" @click="$emit('close')" aria-label="Top control Close modal">
+					<img src="@/assets/icons/x-close.svg" alt="Close" />
+				</button>
+			</div>
 		</template>
 		<template v-slot:[ModalPart.CONTENT]>
-			<h2>{{ selectedProduct.name }}</h2>
-			<img :src="selectedProduct.image" alt="No image" class="modal__image" />
+			<div class="product-modal__content">
+				<div class="product-modal__content-image-wrapper">
+					<img class="image" :src="selectedProduct.image" alt="No image" />
+				</div>
+				<div class="product-modal__content-description">
+					<p class="title">Key features</p>
+					<ul class="features-list">
+						<li v-for="(feature, index) in selectedProduct.keyFeatures" :key="index">
+							{{ feature }}
+						</li>
+					</ul>
+					<p class="description">{{ selectedProduct.description }}</p>
+				</div>
+			</div>
 		</template>
 		<template v-slot:[ModalPart.FOOTER]>
-			<p>{{ selectedProduct.serial }}</p>
+			<div class="product-modal__footer">
+				<button
+					class="product-modal__footer-close-button"
+					@click="$emit('close')"
+					aria-label="Bottom control Close modal"
+				>
+					Close
+				</button>
+			</div>
 		</template>
 	</Modal>
 </template>
+
+<style scoped lang="scss">
+@use '@/assets/styles/_color.vars' as colors;
+@use '@/assets/styles/_mixins' as mixins;
+
+.product-modal {
+	&__header {
+		@include mixins.flex-row(center, space-between);
+
+		width: 100%;
+
+		&-title {
+			@include mixins.font-style(20px, colors.$primary-color, 700);
+
+			line-height: 120%;
+			letter-spacing: 0;
+			margin: 0;
+
+			@media (max-width: 480px) {
+				font-size: 16px;
+			}
+		}
+
+		&-close-button {
+			@include mixins.flex-row();
+
+			width: 40px;
+			height: 40px;
+			border: none;
+			background-color: colors.$snow;
+
+			&:hover {
+				cursor: pointer;
+			}
+		}
+	}
+
+	&__content {
+		@include mixins.flex-row(flex-start, space-between);
+		flex-wrap: wrap;
+
+		width: 100%;
+		padding: 48px 0 80px;
+
+		&-image-wrapper {
+			@include mixins.flex-row();
+
+			width: 314px;
+			height: 303px;
+
+			@media (max-width: 768px) {
+				padding: 24px 0;
+			}
+
+			@media (max-width: 480px) {
+				height: auto;
+				padding: 12px 0;
+				height: 160px;
+				width: 160px;
+			}
+
+			.image {
+				display: block;
+				max-width: 100%;
+				max-height: 100%;
+				object-fit: contain;
+
+				@media (max-width: 480px) {
+					height: auto;
+				}
+
+				@media (max-width: 360px) {
+					width: 88%;
+				}
+			}
+		}
+
+		&-description {
+			@include mixins.flex-column(flex-start, center);
+
+			max-width: 274px;
+
+			.title,
+			.features-list,
+			.description {
+				@include mixins.font-style(12px, colors.$black, 400);
+
+				line-height: 25px;
+				letter-spacing: 0;
+				vertical-align: middle;
+			}
+
+			.title {
+				font-weight: 600;
+			}
+
+			.features-list {
+				padding-left: 24px;
+				margin: 12px 0;
+			}
+		}
+
+		@media (max-width: 668px) {
+			flex-direction: column;
+			align-items: center;
+
+			padding: 24px 0;
+		}
+
+		@media (max-width: 480px) {
+			padding: 12px 0;
+		}
+	}
+
+	&__footer {
+		@include mixins.flex-row(center, flex-end);
+
+		width: 100%;
+
+		&-close-button {
+			@include mixins.font-style(14px, colors.$black, 500);
+
+			width: 88px;
+			height: 44px;
+			padding: 12px 24px;
+			gap: 4px;
+			border: none;
+			border-radius: 4px;
+			background-color: colors.$alabaster;
+
+			line-height: 20px;
+			letter-spacing: 2%;
+			vertical-align: middle;
+
+			&:hover {
+				cursor: pointer;
+			}
+
+			@media (max-width: 480px) {
+				width: 100%;
+				font-size: 12px;
+			}
+		}
+
+		@media (max-width: 668px) {
+			margin-top: auto;
+		}
+	}
+}
+</style>
--- src/shared/components/Modal/Modal.vue
+++ src/shared/components/Modal/Modal.vue
@@ -25,16 +25,47 @@ defineProps<{
 
 	position: fixed;
 	inset: 0;
-	background: rgba(0, 0, 0, 0.6);
 	z-index: 1000;
+
+	width: 100%;
+	height: 100%;
+
+	@media (min-width: 669px) {
+		background: rgba(colors.$black, 0.4);
+	}
 }
 
 .modal {
-	background: white;
-	padding: 24px;
-	border-radius: 12px;
-	width: 90%;
-	max-width: 480px;
-	text-align: center;
+	background-color: colors.$snow;
+	border-radius: 8px;
+	padding: 40px;
+
+	width: 100%;
+	max-width: 684px;
+	max-height: 616px;
+
+	@include mixins.flex-column();
+
+	@media (max-width: 768px) {
+		width: 96vw;
+		padding: 24px;
+	}
+
+	@media (max-width: 668px) {
+		width: 100%;
+		height: calc(100% - 148px);
+		max-height: calc(100% - 148px);
+		position: absolute;
+		bottom: 0;
+		border-radius: 0px;
+	}
+
+	@media (max-width: 560px) {
+		padding: 16px;
+	}
+
+	@media (max-width: 480px) {
+		padding: 12px;
+	}
 }
 </style>
